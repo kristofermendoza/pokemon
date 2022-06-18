a2d4c98c3bb760cbaf31383e276924341486ffb4
kristofermendoza <kris06mendoza@gmail.com>
2022-06-18 12:25:00 -0700

    update

---
+++ src/components/Nav.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './index.css'
+
+const Nav = () => {
+    return (
+        <nav>
+            <div className='container'>
+                <a className='item1' href="/">Pokemon Search</a>
+                <a className='item2' href="/">About</a>
+                <a className='item3' href="/">Coming soon...</a>
+            </div>
+        </nav>
+    )
+}
+
+
+
+
+
+
+export default Nav;
