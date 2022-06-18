6857ef0f4e290d4b795e7144993551ada646bec8
kristofermendoza <kris06mendoza@gmail.com>
2022-06-18 16:23:20 -0700

    navbar layout

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,21 +1,28 @@
-import React from 'react'
-import './index.css'
+import React from "react";
 
 const Nav = () => {
-    return (
-        <nav>
-            <div className='container'>
-                <a className='item1' href="/">Pokemon Search</a>
-                <a className='item2' href="/">About</a>
-                <a className='item3' href="/">Coming soon...</a>
-            </div>
-        </nav>
-    )
-}
-
-
-
-
-
+  return (
+    <nav>
+      <div className="big__container">
+        <div className="sm__container-1">
+          <div className="title" href="/">
+            Pokemon Search
+          </div>
+          <ul className="navbar__items">
+            <li className="navbar__item">
+              <a href="/">About</a>
+            </li>
+            <li className="navbar__item">
+              <a href="/">Coming soon...</a>
+            </li>
+          </ul>
+        </div>
+        <div className="sm__container-2">
+          <input type="search" placeholder="Search for Pokemon here!" />
+        </div>
+      </div>
+    </nav>
+  );
+};
 
 export default Nav;
